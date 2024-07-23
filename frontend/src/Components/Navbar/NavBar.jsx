629d11dfb68803e78bd522228f4cc296d5c7ce92
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-07-22 23:58:33 -0500

    added nav bar item

--- frontend/src/Components/Navbar/NavBar.jsx
+++ frontend/src/Components/Navbar/NavBar.jsx
@@ -1,10 +1,25 @@
 import React from "react"
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () =>{
     return(
         <div className="navbar">
-            
+            <div className="nav-logo">
+                <img src={logo} alt=""/>
+                <p>YarnLoop</p>
+            </div>
+            <ul className="nav-menu">
+                <li>All</li>
+                <li>Clothes</li>
+                <li>Animal</li>
+                <li>Food</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon} alt="" />
+            </div>
         </div>
     )
 }
